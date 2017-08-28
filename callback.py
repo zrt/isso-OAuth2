9d43f303ce76ac5c706141037626f615fe290ddf
Ruotian <me@zrt.io>
2017-08-28 21:38:01 +0800

    aaa

--- callback.py
+++ callback.py
@@ -1,7 +1,7 @@
 from flask import Flask,request,redirect
 import config,json
 from urllib.parse import quote_plus
-from app import sign
+from app import sign,send_comment
 app = Flask('callback')
 
 app.debug = False
@@ -12,7 +12,31 @@ app.debug = False
 
 @app.route('/',methods=['GET'])
 def callback():
-	print(request.args)
+	# print(request.args)
+	arg = dict(request.args)
+	if 'state' not in arg:
+		return '[!] OAuth failed. code:0'
+	if 'content' not in arg:
+		return '[!] OAuth failed. code:1'
+	if 'code' not in arg:
+		return '[!] OAuth failed. code:3'
+
+	if arg['state'] != sign(arg['content']):
+		return '[!] OAuth failed. code:4'
+
+	content = json.loads(arg['content'])
+	code = arg['code']
+	r = request.post('https://github.com/login/oauth/access_token',
+		data={'client_id':config.client_id,
+		'client_secret':config.client_secret,
+		'code':code},
+		headers={'Accept':'application/json'})
+	r = r.json()
+	if 'access_token' not in r:
+		return '[!] OAuth failed. code:5'
+	token = r['access_token']
+	r = request.get('https://api.github.com/user',auth=('token',token))
+	print(r.json())
 	return 'ok'
 
 
